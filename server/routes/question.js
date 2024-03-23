826c8e430bd4dedcdaf17a03dacbc2d69f1e01cd
Arnab Karmakar <117086972+ArnabKarmakar1108@users.noreply.github.com>
2024-03-23 16:19:27 +0530

    latest update at 4:20

---
+++ server/routes/question.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const questions = require("../controllers/question.js");
+
+const router = express.Router();
+
+// GET ALL QUESTIONS
+router.get("", questions.getAllQuestions);
+
+// GET QUESTIONS BY USERID
+router.get("/myQuestions/:userId", questions.getMyQuestions);
+
+// GET A QUESTION BY QUESTIONID
+router.get("/myQuestion/:questionId", questions.getMyQuestion);
+
+// CREATE A QUESTION
+router.post("/createQuestion", questions.createQuestion);
+
+// UPDATE A QUESTION
+router.put("/updateQuestion/:questionId", questions.updateQuestion);
+
+// DELETE A QUESTION
+router.delete("/deleteQuestion/:questionId", questions.deleteQuestion);
+
+module.exports = router
