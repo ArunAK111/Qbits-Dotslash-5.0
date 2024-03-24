688e795b8a8e1aaf726f49db49b1c27f84a25984
Arun Kurali <117448626+ArunAK111@users.noreply.github.com>
2024-03-24 09:48:35 +0530

    Add files via upload

--- client/src/components/mainpage_details/MainpageDetails.js
+++ client/src/components/mainpage_details/MainpageDetails.js
@@ -1,76 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from '../navbar/Navbar';
-import './mainpagedetails.css';
-import { useParams } from 'react-router-dom';
-import { getQuestionById } from '../../api/question';
-import { pdfjs } from 'react-pdf';
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.js',
-    import.meta.url,
-).toString();
+import React, { useEffect, useState } from "react";
+import Navbar from "../navbar/Navbar";
+import "./mainpagedetails.css";
+import { useParams } from "react-router-dom";
+import { getQuestionById } from "../../api/question";
 
 const MainpageDetails = () => {
-    const { postId } = useParams();
-    const [question, setQuestion] = useState(null);
-    const [test, setTest] = useState(null);
+  const { postId } = useParams();
+  const [question, setQuestion] = useState(null);
 
-    // make an api call using the postId and map over the questions array
+  function convertToNewUrl(originalUrl) {
+    // Assuming the original URL is of the form "https://dsld.od.nih.gov/label/{id}.pdf"
+    const id = originalUrl.split("/").pop().replace(".pdf", "");
+    // Correctly form the new URL without duplicating parts of the path
+    return `https://api.ods.od.nih.gov/dsld/s3/pdf/${id}.pdf`;
+  }
 
-    function convertToNewUrl(originalUrl) {
-        const prefix = 'https://dsld.od.nih.gov/label/';
-        const suffix = '.pdf';
-        const id = originalUrl.substring(prefix.length);
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      getQuestionById(postId)
+        .then((res) => res.json())
+        .then((data) => setQuestion(data));
+    };
+    fetchQuestion();
+  }, [postId]);
 
-        return `https://api.ods.od.nih.gov/dsld/s3/pdf/${id}${suffix}`;
-    }
-
-    useEffect(() => {
-        const fetchQuestion = async () => {
-            getQuestionById(postId).then(res => res.json()).then(data => setQuestion(data));
-
-        };
-        fetchQuestion();
-    }, [])
-
-    // const [shouldReload, setShouldReload] = useState(true);
-
-    // useEffect(() => {
-    //     if (shouldReload) {
-    //         // Reload the page when the component mounts
-    //         window.location.reload();
-    //         // Set shouldReload to false to prevent continuous reloads
-    //         setShouldReload(false);
-    //     }
-    // }, [shouldReload]);
-
-    return (
-        <div className='mainpage_details'>
-            <Navbar />
-            {question && (
-                <>
-                    <div className='mainpage_details_top'>
-                        <h1>Recommendations ({question.rec_list.length})</h1>
-                    </div>
-                    <div className='mainpage_details_container'>
-                        {question.rec_list.map((d) => (
-                            <div className='mainpage_details_inner'>
-                                <div className='mainpage_details_object'>
-                                    <embed src={convertToNewUrl(d[0])} type="application/pdf" width="100%" height="100%" />
-                                </div>
-                                <div className='mainpage_details_object_bottom'>
-                                    <h3>{d[2]}</h3>
-                                    <p>By {d[3]}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </>
-            )}
-
-
-        </div>
-    )
-}
+  return (
+    <div className="mainpage_details">
+      <Navbar />
+      {question && (
+        <>
+          <div className="mainpage_details_top">
+            <h1>Recommendations ({question.rec_list.length})</h1>
+          </div>
+          <div className="mainpage_details_container">
+            {question.rec_list.map((d, index) => (
+              <div key={index} className="mainpage_details_inner">
+                <div className="mainpage_details_object">
+                  {/* Instead of embedding, provide a link to the PDF */}
+                  <a
+                    href={convertToNewUrl(d[0])}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View Document
+                  </a>
+                </div>
+                <div className="mainpage_details_object_bottom">
+                  <h3>{d[2]}</h3>
+                  <p>By {d[3]}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default MainpageDetails;
